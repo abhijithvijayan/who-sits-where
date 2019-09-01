04f535eac8366de85919b121afaa79186a04db69
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-09-01 18:01:40 +0530

    add home screen cubes

--- src/screens/main/HomeScreen.js
+++ src/screens/main/HomeScreen.js
@@ -6,13 +6,51 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'row',
+  },
+  light: {
+    width: 75,
+    height: 75,
+    backgroundColor: 'powderblue',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sky: {
+    width: 75,
+    height: 75,
+    backgroundColor: 'skyblue',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bright: {
+    width: 75,
+    height: 75,
+    backgroundColor: 'steelblue',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
 
 const HomeScreen = () => {
   return (
     <View style={styles.main}>
-      <Text>Hello World</Text>
+      <View style={styles.bright}>
+        <Text style={styles.text}>Abhijith</Text>
+      </View>
+      <View style={styles.sky}>
+        <Text style={styles.text}>Lijin</Text>
+      </View>
+      <View style={styles.light}>
+        <Text style={styles.text}>Fairooz</Text>
+      </View>
     </View>
   );
 };
